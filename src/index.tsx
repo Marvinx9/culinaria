e7a425ab7fef3b06c007a2f738fa26f9812900f2
Afranio Nunes Dantas <afraniodantas0224@gmail.com>
2025-02-06 22:46:53 -0300

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css';
+import './index.css';
+import Inicio from 'pages/Inicio';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Inicio />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
